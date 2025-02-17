ab87405d3fc07a6077b776fe1ccd7e375f10bc49
קמפנר אסתר <214991887@main.local>
2025-02-17 12:03:22 +0200

    three

--- projectAI/BL/Services/BlServicesOreder.cs
+++ projectAI/BL/Services/BlServicesOreder.cs
@@ -17,11 +17,13 @@ namespace bl.Services
         public List<Order> GetListorders() 
         {
             //צריך פה לשנות וללכת לDAL ולקחת משם את הרשימה 
-            Movie m=new Movie(7767,"הפונציקים","סרט לילדים","www.ch.il","הצגות",100,12/12/2000,AgeGroup.Children);
-            User u=new User(21321,"tmp","tmp@","password",54,eGender.male);
-            Order o=new Order(21123,u,);
-                List<Order>lst=new List<Order>();
-                return
+            Movie m=new Movie(7767,"IMG","הפונציקים","סרט לילדים","www.ch.il","הצגות",100,12/12/2000,AgeGroup.Children,false);
+            User u=new User(21321,"IMG","tmp","tmp@","password",54,eGender.male);
+            Order o = new Order(21123, u, m, DateTime.Now, Statuse.Oreder);
+
+            List<Order>lst=new List<Order>();
+            lst.Add(o);
+            return lst;
         }
     }
 }
